fc820eb5b1c30796b411c2c7a207b1e1275cb321
Lina Urdaneta <pixelada@gmail.com>
2023-07-07 10:57:15 -0400

    Reordering comments

--- 54-55-quiz/before/script.js
+++ 54-55-quiz/before/script.js
@@ -1,35 +1,34 @@
-//Select all elements needed
-
+//Selecting all elements needed
 const form = document.querySelector("#quiz-form");
 const answers = document.querySelectorAll(".answer");
 const questions = document.querySelectorAll(".question-item");
 const alert = document.querySelector("#alert");
 
-//Create a submit event listener for the form that does the following.
-//Prevent the default behaviour
-//Get all selected answers (use the `checked` property on the input to determine if it is selected or not)
-//Loop through the selected answer to see if they are correct or not (Check the value of the answer to see if it is the string "true")
-//For each correct answer add the class `correct` to the parent with the class `question-item` and remove the class `incorrect`.
-//For each incorrect answer add the class `incorrect` to the parent with the class `question-item` and remove the class `correct`.
-//Make sure unanswered questions show up as incorrect. The easiest way to do this is to add the incorrect class and removing the correct class from all question items before checking the correct answers
-//If all answers are correct show the element with the id `alert` and hide it after one second (look into setTimeout) (use the class active to show the alert and remove the class to hide it)
-
+//Submit event listener for the form
 form.addEventListener("submit", (e) => {
+	//Preventing the default behaviour
 	e.preventDefault();
+
+	//Making sure unanswered questions show up as incorrect
 	let i = 0;
 	questions.forEach((question) => {
 		question.classList.add("incorrect");
 		question.classList.remove("correct");
 	});
 
+	//Getting all selected answers
 	answers.forEach((answer) => {
 		const parent = answer.closest(".question-item");
 
 		if (answer.checked) {
+			//Looping through the selected answer to see if they are correct or not
 			if (answer.value === "true") {
 				i++;
+				//For each correct answer, adding the class `correct` to the parent with the class `question-item` and remove the class `incorrect`
 				parent.classList.remove("incorrect");
 				parent.classList.add("correct");
+
+				//If all answers are correct, showing the element with the id `alert` and hide it after one second
 				if (i === 3) {
 					alert.classList.add("active");
 					setTimeout(() => {
@@ -37,6 +36,7 @@ form.addEventListener("submit", (e) => {
 					}, "1000");
 				}
 			} else {
+				//For each incorrect answer, adding the class `incorrect` to the parent with the class `question-item` and remove the class `correct`.
 				parent.classList.add("incorrect");
 				parent.classList.remove("correct");
 			}
