fb21b1eefd8ec986ea721405dff42e79b71c10d1
Lina Urdaneta <pixelada@gmail.com>
2023-07-07 12:15:59 -0400

    Improved solution

--- 54-55-quiz/before/script.js
+++ 54-55-quiz/before/script.js
@@ -1,6 +1,6 @@
 //Selecting all elements needed
 const form = document.querySelector("#quiz-form");
-const answers = document.querySelectorAll(".answer");
+const answers = Array.from(document.querySelectorAll(".answer"));
 const questions = document.querySelectorAll(".question-item");
 const alert = document.querySelector("#alert");
 
@@ -10,37 +10,38 @@ form.addEventListener("submit", (e) => {
 	e.preventDefault();
 
 	//Making sure unanswered questions show up as incorrect
-	let i = 0;
 	questions.forEach((question) => {
 		question.classList.add("incorrect");
 		question.classList.remove("correct");
 	});
 
 	//Getting all selected answers
-	answers.forEach((answer) => {
+	const checkedAnswers = answers.filter((answer) => answer.checked);
+
+	//Looping through the selected answer to see if they are correct or not
+	checkedAnswers.forEach((answer) => {
+		const isCorrect = answer.value === "true";
 		const parent = answer.closest(".question-item");
 
-		if (answer.checked) {
-			//Looping through the selected answer to see if they are correct or not
-			if (answer.value === "true") {
-				i++;
-				//For each correct answer, adding the class `correct` to the parent with the class `question-item` and remove the class `incorrect`
-				parent.classList.remove("incorrect");
-				parent.classList.add("correct");
+		//For each correct answer, adding the class `correct` to the parent with the class `question-item` and remove the class `incorrect`
+		if (isCorrect) {
+			parent.classList.remove("incorrect");
+			parent.classList.add("correct");
+		} else {
+			//For each incorrect answer, adding the class `incorrect` to the parent with the class `question-item` and remove the class `correct`.
+			parent.classList.add("incorrect");
+			parent.classList.remove("correct");
+		}
 
-				//If all answers are correct, showing the element with the id `alert` and hide it after one second
-				if (i === 3) {
-					alert.classList.add("active");
-					setTimeout(() => {
-						alert.classList.remove("active");
-					}, "1000");
-				}
-			} else {
-				//For each incorrect answer, adding the class `incorrect` to the parent with the class `question-item` and remove the class `correct`.
-				parent.classList.add("incorrect");
-				parent.classList.remove("correct");
-			}
-			//change
+		//If all answers are correct, showing the element with the id `alert` and hide it after one second
+		const allTrue = checkedAnswers.every((answer) => answer.value === "true");
+		const allAnswered = checkedAnswers.length === questions.length;
+
+		if (allTrue && allAnswered) {
+			alert.classList.add("active");
+			setTimeout(() => {
+				alert.classList.remove("active");
+			}, "1000");
 		}
 	});
 });
